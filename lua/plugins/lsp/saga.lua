2f5a943ddede78d4432a2d8cb1ad149234631f46
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-09-01 13:14:38 +0300

    Update lspsaga config

--- lua/plugins/lsp/saga.lua
+++ lua/plugins/lsp/saga.lua
@@ -4,16 +4,16 @@ if not status_ok then
 end
 
 saga.setup({
-  lightbulb = {
-    enable = true,
-    enable_in_insert = false,
-    sign = false,
-    sign_priority = 40,
-    virtual_text = true,
-  },
-  hover = {
-    max_width = 0.6,
-    open_link = "gx",
-    open_browser = "!chrome",
-  },
+  -- lightbulb = {
+  --   enable = true,
+  --   enable_in_insert = false,
+  --   sign = false,
+  --   sign_priority = 40,
+  --   virtual_text = true,
+  -- },
+  -- hover = {
+  --   max_width = 0.6,
+  --   open_link = "gx",
+  --   open_browser = "!chrome",
+  -- },
 })
