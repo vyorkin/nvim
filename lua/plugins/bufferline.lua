661dbac95afaeaeab949d2b48ab7faf191d7bf02
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2024-11-26 16:14:57 +0700

    Update config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,10 +2,11 @@ return {
   "akinsho/bufferline.nvim",
   event = "VeryLazy",
   enabled = false,
-  opts = {
-    options = {
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-    },
-  },
+  opts = function(_, opts)
+    if (vim.g.colors_name or ""):find("catppuccin") then
+      opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+    end
+    opts.show_buffer_close_icons = false
+    opts.show_close_icon = false
+  end,
 }
