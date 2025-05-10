664db68951b249526cff46a22f0d8ed475662212
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-05-10 12:37:35 +0300

    Updates

---
+++ lua/plugins/augment.lua
@@ -0,0 +1,17 @@
+-- enable/disable augment based on the cwd
+-- dynamically set the workspace directory
+
+return {
+  "augmentcode/augment.vim",
+  enabled = function()
+    local cwd = vim.loop.cwd()
+    local file = cwd .. "/.augmentignore"
+    local enabled = vim.loop.fs_stat(file) ~= nil
+    return enabled
+  end,
+  config = function()
+    local cwd = vim.loop.cwd()
+    vim.g.augment_workspace_folders = { cwd }
+    -- key binds
+  end,
+}
